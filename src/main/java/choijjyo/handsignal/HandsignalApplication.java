5bdc7dbb0a445b5359a1485f56d9a4a73241b056
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-06-20 13:56:12 +0900

    Initial settings

---
+++ src/main/java/choijjyo/handsignal/HandsignalApplication.java
@@ -0,0 +1,13 @@
+package choijjyo.handsignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsignalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/choijjyo/handsignal/HandsignalApplicationTests.java
@@ -0,0 +1,13 @@
+package choijjyo.handsignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
