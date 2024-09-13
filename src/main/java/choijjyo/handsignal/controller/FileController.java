706f18a40b06ef55bca010856c388786522fbc7c
givesilverstick <silverstick21@sookmyung.ac.kr>
2024-09-14 02:04:03 +0900

    Feat: S3 CORS 설정

--- src/main/java/choijjyo/handsignal/controller/FileController.java
+++ src/main/java/choijjyo/handsignal/controller/FileController.java
@@ -15,6 +15,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/files")
 @RequiredArgsConstructor
